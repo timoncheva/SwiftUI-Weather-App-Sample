fd3d1e9d0ff48b4a1b761976382b121c876147e6
timoncheva <miazina.elle@gmail.com>
2022-11-12 15:56:52 +0300

    Initial Commit

---
+++ Weather Application/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather Application
+//
+//  Created by Ella on 12.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Application/Weather_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ApplicationApp.swift
+//  Weather Application
+//
+//  Created by Ella on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
